d36466fd67a392aec802a685f65bcaa992de8adf
Emanuel <emanuelmisic64@gmail.com>
2023-02-21 21:49:19 +0100

    align start button

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ clock = pygame.time.Clock()
 start_img = pygame.image.load('assets\start_game_btn.png').convert_alpha()
 
 #create button instances
-start_button = Button(100, 200, start_img, 0.9)
+start_button = Button(WIDTH / 2 - 140, HEIGHT / 2 - 300, start_img, 1)
 
 class Game:
     def __init__(self, level_num):
@@ -43,8 +43,8 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pass
+                # if event.type == pygame.MOUSEBUTTONDOWN:
+                #     pass
                     # game = Game(1)
                     # game.run()
 
