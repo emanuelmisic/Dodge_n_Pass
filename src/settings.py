ad77b73c9de0e819bad9cf6e3d827ad68f7b1e92
Emanuel <emanuelmisic64@gmail.com>
2023-02-26 17:24:52 +0100

    add pause_bg

--- src/settings.py
+++ src/settings.py
@@ -3,17 +3,17 @@ import pygame
 
 from enum import Enum
 
-screen = pygame.display.set_mode((864, 864))
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-
 BG1 = pygame.image.load(os.path.join('assets', 'background_1.png'))
 
 HEIGHT = 864
 WIDTH = 864
 FPS = 60
 TILE_SIZE = 48
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pause_bg = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+pause_bg.fill((0, 0, 0, 80))
 
 class GameState(Enum):
     START = 'START'
